90c01c478d1ea880d0627ceeb833c0b273d4888e
Logan Cunningham <lc2523@hwsrv-1244939.hostwindsdns.com>
2025-02-07 23:03:19 +0000

    Im losing it

---
+++ main.c
@@ -0,0 +1,19 @@
+#include "nqueens.h"
+#include <stdio.h>
+#include "solver.h"
+
+int main() {
+    int n;
+    printf("Enter the value of N: ");
+    scanf("%d", &n);
+
+    if (n < 1) {
+        printf("Invalid value of N. Please enter a number greater than 0.\n");
+        return 1;
+    }
+
+    solve_nqueens(n);
+
+    return 0;
+}
+
